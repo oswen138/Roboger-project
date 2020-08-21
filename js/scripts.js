356c141f6e296c9554dfddcab365902b9861c6a8
ophelia swen <ophelia.swen@gmail.com>
2020-08-21 11:38:15 -0700

    add test #4

--- js/scripts.js
+++ js/scripts.js
@@ -45,9 +45,45 @@ function number(4) {
 
 Test #2
 
-Test: numbers that contain a 1
+Test: "numbers that contain 1"
 Expect(number(31)).toEqual("Beep!");
 
-function number(31) {
-  if (number.includes('1')
+function number(21) {
+  if (number.includes('1') {
+    return beep!;
+  } else {
+    return Moo; 
+  }
+  }
 }
+
+Test #3
+
+Test: "numbers that contain 2"
+Expect(number(32)).toEqual("Boop!");
+
+function number(32) {
+  if (number.includes('2') {
+    return "Boop";
+  } else if (number.includes('1') {
+    return "Beep";
+  }
+}
+
+Test#4
+
+Test: "numbers that contain 3"
+Expect(number(13)).toEqual("Won't you be my neighbor?");
+
+function number(13) {
+  if (number.includes('3') {
+    return "Won't you be my neighbor?";
+  } else if (number.includes('1') {
+    return "Beep";
+  } else if (number.includes('2') {
+    return "Boop";
+  } else {
+    return "Moo!"
+  }
+}
+
