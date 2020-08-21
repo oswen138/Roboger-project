61137aacbf61dd325c3facfe1b928ffb3a58362d
ophelia swen <ophelia.swen@gmail.com>
2020-08-21 12:01:44 -0700

    test out ways to write the UI logic and make it work

--- js/scripts.js
+++ js/scripts.js
@@ -18,13 +18,14 @@ function userArray (numberStop) {
   return numbers;
 }
 
+is this working event,,,
 
 // User Interface Logic
 $(document).ready(function() {
     $("form#formOne").submit(function(event) {
     event.preventDefault();
-    var numberStop = ($("input#number").val()); 
-    var result = userArray(numberStop);
+    const number = ($("input#number").val()); 
+    const result = userArray(numberStop);
       $("#outputPhrase").show();
       $("#outputNumbers").text(result);
      
@@ -32,58 +33,58 @@ $(document).ready(function() {
     
 });
 
-<! -- This is pseudo-code testing -->
+// <! -- This is pseudo-code testing -->
 
-Describe: number()
-Test: start at 0, numbers run upwards in increments of 1
-Code: i=0; i < number; i++
-Expect(num(4)).toEqual(0,1,2,3,4);
+// Describe: number()
+// Test: start at 0, numbers run upwards in increments of 1
+// Code: i=0; i < number; i++
+// Expect(num(4)).toEqual(0,1,2,3,4);
 
-function number(4) {
-  return 0,1,2,3,4
-}
+// function number(4) {
+//   return 0,1,2,3,4
+// }
 
-Test #2
+// Test #2
 
-Test: "numbers that contain 1"
-Expect(number(31)).toEqual("Beep!");
+// Test: "numbers that contain 1"
+// Expect(number(31)).toEqual("Beep!");
 
-function number(21) {
-  if (number.includes('1') {
-    return beep!;
-  } else {
-    return Moo; 
-  }
-  }
-}
+// function number(21) {
+//   if (number.includes('1') {
+//     return beep!;
+//   } else {
+//     return Moo; 
+//   }
+//   }
+// }
 
-Test #3
+// Test #3
 
-Test: "numbers that contain 2"
-Expect(number(32)).toEqual("Boop!");
+// Test: "numbers that contain 2"
+// Expect(number(32)).toEqual("Boop!");
 
-function number(32) {
-  if (number.includes('2') {
-    return "Boop";
-  } else if (number.includes('1') {
-    return "Beep";
-  }
-}
+// function number(32) {
+//   if (number.includes('2') {
+//     return "Boop";
+//   } else if (number.includes('1') {
+//     return "Beep";
+//   }
+// }
 
-Test#4
+// Test#4
 
-Test: "numbers that contain 3"
-Expect(number(13)).toEqual("Won't you be my neighbor?");
+// Test: "numbers that contain 3"
+// Expect(number(13)).toEqual("Won't you be my neighbor?");
 
-function number(13) {
-  if (number.includes('3') {
-    return "Won't you be my neighbor?";
-  } else if (number.includes('1') {
-    return "Beep";
-  } else if (number.includes('2') {
-    return "Boop";
-  } else {
-    return "Moo!"
-  }
-}
+// function number(13) {
+//   if (number.includes('3') {
+//     return "Won't you be my neighbor?";
+//   } else if (number.includes('1') {
+//     return "Beep";
+//   } else if (number.includes('2') {
+//     return "Boop";
+//   } else {
+//     return "Moo!"
+//   }
+// }
 
