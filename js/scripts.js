849db264e28260e67c6ceb7ca3b305d474a826ef
ophelia swen <ophelia.swen@gmail.com>
2020-08-21 10:16:57 -0700

    add js script

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+// Back End Logic
+function userArray (numberStop) { 
+  var numbers = [];
+  for (let index = 0; index <= numberStop; index += 1) {
+    var words = index.toString();
+
+    if (words.includes(3)) {
+      numbers.push("Won't you be my neighbor?");
+    } else if (words.includes(2)) {
+      numbers.push("Boop");
+    } else if (words.includes(1)) {
+      numbers.push("Beep");
+    } else {
+      numbers.push(words);
+    }
+    console.log("This is the index for every loop: " +index);
+  }
+  return numbers;
+}
+
+
+// User Interface Logic
+$(document).ready(function() {
+    $("form#neighbor").submit(function(event) {
+    event.preventDefault();
+    var numberStop = ($("input#number").val()); 
+    var result = userArray(numberStop);
+      $("#outputPhrase").show();
+      $("#outputNumbers").text(result);
+     
+    });
+    
+});
