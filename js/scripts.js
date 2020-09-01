3212d25bbc59f1c48af4068ab66945f4f45f12b0
ophelia swen <ophelia.swen@gmail.com>
2020-08-31 19:17:13 -0700

    rewrite code so shalala paragraph goes away after answer input

--- js/scripts.js
+++ js/scripts.js
@@ -4,31 +4,31 @@ function speakArray(input) {
   const inputNum = [];
   for (let i=0; i <= input; i++) {
   
-    if (i.toString().includes("1")) {
-      inputNum.push("Beep!");
+    if (i.toString().includes("3")) {
+      inputNum.push("Won't you be my neighbor?");
     }
     else if (i.toString().includes("2")) {
       inputNum.push("Boop!");
     }
-    else if (i.toString().match("3")) {
-      inputNum.push("Won't you be my neighbor?");
+    else if (i.toString().includes("1")) {
+      inputNum.push("Beep!");
     } else {
       inputNum.push(i);
     }
   }
   return inputNum;
-};
+}
+
 
 //Front end Logic
 
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
+  $("#formZing").submit(function(event) {
     event.preventDefault();
     const inputNum  = parseInt($("#input").val());
-    let result = speakArray(inputNum).toString;
+    let result = speakArray(inputNum).toString();
     $("#result").text(result);
     $("#result").show() 
-
   });
 });
     
